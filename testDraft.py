e7934043f97966e53365b8992330a70a78722be1
xinyaoli <li651854292@gmail.com>
2023-08-05 16:17:02 -0700

    Test the bybit api

--- testDraft.py
+++ testDraft.py
@@ -1,21 +1,15 @@
 from inputs import config
 from inputs import parameter
 from inputs import variable
-from pybitget import Client
+import bybit
 import logging
 import time
 
-#response = requests.get("https://api/spot/v1/trade/orders?symbol=BTCUSDT&price=274265&&quantity=0.001&side=buy&orderType=limit&force=normal")
-#result = response.json()
-#print(result)
 
-client = Client(config.API_KEY, config.SECRET_KEY, config.API_PASSPHARSE)
-#buy_order = client.spot_place_order(symbol='btcusdt_spbl', price='31426.5', quantity='0.0010000914588683622', side='buy', orderType='limit',force="normal")
-#sell_order = client.spot_place_order(symbol='btcusdt_spbl', price='29426.5', quantity='0.005', side='sell', orderType='limit',force="normal")
-#print(buy_order)
-#print(sell_order)
-print(client.spot_get_account_assets())
-#print(client.mix_get_accounts('UMCBL'))
-#print(client.mix_get_single_symbol_ticker('BTCUSDT_UMCBL').get('data').get('bestAsk'))
-#print(client.mix_get_single_symbol_ticker('BTCUSDT_UMCBL').get('data').get('bestBid'))
-#print(sell_order)
+client =bybit.bybit(test=False,api_key=config.API_KEY,api_secret=config.SECRET_KEY)
+info = client.Market.Market_symbolInfo().result()
+keys = info[0]['result']
+balance = client.Wallet.Wallet_getBalance(coin='BTC').result()
+btc = keys[0]['last_price']
+print(balance)
+print(keys[0])
