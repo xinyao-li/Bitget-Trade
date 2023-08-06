fa60a3c6e42a70d6040fb4b3398f4c33e636102b
xinyaoli <li651854292@gmail.com>
2023-08-05 20:56:35 -0700

    Test the bybit api

--- inputs/variable.py
+++ inputs/variable.py
@@ -1,4 +1,4 @@
 last_trade_price=26308.0
-seconds=44
+seconds=62
 buy_balance=3
 sell_balance=1
--- testDraft.py
+++ testDraft.py
@@ -6,10 +6,14 @@ import logging
 import time
 
 
-client =bybit.bybit(test=False,api_key=config.API_KEY,api_secret=config.SECRET_KEY)
+client = bybit.bybit(test=False,api_key=config.API_KEY,api_secret=config.SECRET_KEY)
 info = client.Market.Market_symbolInfo().result()
 keys = info[0]['result']
 balance = client.Wallet.Wallet_getBalance(coin='BTC').result()
-btc = keys[0]['last_price']
+btc_usd_index = 51
+btc = keys[btc_usd_index]['last_price']
+pos = client.Positions.Positions_myPosition(symbol="BTCUSD").result()
+print(pos)
 print(balance)
-print(keys[0])
+print(keys[btc_usd_index])
+print(client.Order.Order_new(side="Buy",symbol="BTCUSD",order_type="Limit",qty=0.0001,price=btc,time_in_force="GoodTillCancel").result())
