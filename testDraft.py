16e10f4f81e23aeb5231214d043b94c687acbdba
xinyaoli <li651854292@gmail.com>
2023-08-23 01:04:08 -0700

    Pybit api testing and api key update

--- testDraft.py
+++ testDraft.py
@@ -1,19 +1,48 @@
 from inputs import config
-from inputs import parameter
-from inputs import variable
-import bybit
-import logging
-import time
+from pybit.unified_trading import HTTP
+
+session = HTTP(
+    testnet=False,
+    api_key=config.API_KEY,
+    api_secret=config.SECRET_KEY
+)
+#Get the buying power in derivatives
+print(session.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
+
+print(session.get_wallet_balance(accountType="CONTRACT",coin='BTC'))
+
+bid_price = session.get_tickers(category="linear",symbol="BTCUSDT")['result']['list'][0]['bid1Price']
+ask_price = session.get_tickers(category="linear",symbol="BTCUSDT")['result']['list'][0]['ask1Price']
+print(bid_price)
+print(ask_price)
+
+#Place a buy order
+''''
+print(session.place_order(
+    category="linear",
+    symbol="BTCUSDT",
+    side="Buy",
+    orderType="Limit",
+    qty="0.01",
+    price=ask_price,
+    positionIdx ="1",
+    timeInForce="GTC",
+))
+'''
+#Place a sell order
+'''
+print(session.place_order(
+    category="linear",
+    symbol="BTCUSDT",
+    side="Sell",
+    orderType="Limit",
+    qty="0.01",
+    price=bid_price,
+    positionIdx ="2",
+    timeInForce="GTC",
+))
+'''
 
 
-client = bybit.bybit(test=False,api_key=config.API_KEY,api_secret=config.SECRET_KEY)
-info = client.Market.Market_symbolInfo().result()
-keys = info[0]['result']
-balance = client.Wallet.Wallet_getBalance(coin='BTC').result()
-btc_usd_index = 51
-btc = keys[btc_usd_index]['last_price']
-pos = client.Positions.Positions_myPosition(symbol="BTCUSD").result()
-print(pos)
-print(balance)
-print(keys[btc_usd_index])
-print(client.Order.Order_new(side="Buy",symbol="BTCUSD",order_type="Limit",qty=0.0001,price=btc,time_in_force="GoodTillCancel").result())
+
+
